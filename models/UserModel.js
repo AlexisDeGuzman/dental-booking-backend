9dc285f9e3ef7a2ec5860f1663ecb424d4d44f44
deguzmanalexisprexb <deguzmanalexisprexb@gmail.com>
2023-08-02 14:32:11 +0800

    Create UserModel with inital schema

---
+++ models/UserModel.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema ({
+	firstName : {
+		type: String,
+		required : [true, "Firs Name is required"]
+	},
+	lastName : {
+		type: String,
+		required : [true, "Last Name is required"]
+	},
+	address : {
+		type: String,
+		required : [true, "Address is required"]
+	},
+	contactNumber : {
+		type: String,
+		required : [true, "Contact Number is required"]
+	},
+	email : {
+		type: String,
+		required : [true, "Email is required"]
+	},
+	password : {
+		type: String,
+		required : [true, "Email is required"]
+	},
+	isAdmin : {
+		type: Boolean,
+		default : false,
+		required : [true, "isAdmin is required"]
+	},
+	appointments: [
+		{
+			appointmentId: {
+				type: String,
+				required : [true, "Appointment ID is required"]
+			},
+			date: {
+				type: String,
+				required : [true, "Date is required"]
+			},
+			time: {
+				type: String,
+				required : [true, "Date is required"]
+			}
+		}
+	]
+})
+
+module.exports = mongoose.model("User", userSchema);
