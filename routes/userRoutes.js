2e674f5d35b4c41b43f48162902215f922fe0ce3
deguzmanalexisprexb <deguzmanalexisprexb@gmail.com>
2023-08-02 15:28:29 +0800

    Added register and login in userRoutes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const userController = require("../controllers/userController");
 
+// register
+router.post("/register", userController.registerUser);
 
+// login
+router.post("/login", userController.loginUser);
 
 module.exports = router;
