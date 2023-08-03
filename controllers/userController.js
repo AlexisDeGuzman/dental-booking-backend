011b582693f94793d85791ee7eee586a548f9a1f
deguzmanalexisprexb <deguzmanalexisprexb@gmail.com>
2023-08-03 16:27:40 +0800

    Added getUserDetails in controllers and routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -68,3 +68,23 @@ module.exports.loginUser = (req, res) => {
 		}
 	})
 };
+
+// Retrieve User Details
+module.exports.getUserDetails = async (req, res) => {
+	try {
+		const { id: userId, isAdmin } = auth.decode(req.headers.authorization);
+
+		const user = await User.findById(userId);
+		if (user) {
+			user.password = "";
+
+		res.send(user);
+
+		} else {
+			res.status(404).send("User not found");
+		}
+
+	} catch (error) {
+		res.status(500).send(error.message);
+	}
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
+const auth = require("../auth")
 
 // register
 router.post("/register", userController.registerUser);
 
 // login
 router.post("/login", userController.loginUser);
+
+// retrieve user details
+router.get("/userDetails", auth.verify, userController.getUserDetails);
 
 module.exports = router;
